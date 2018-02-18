39eb71be2fb2f948a0f84b33000f37e3cb925bf7
Denise Pendleton <dpendl1@hotmail.com>
2018-02-18 22:40:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
       "NO, NOT SINCE 1938!"
 
     elseif phrase == "I LOVE YOU GRANDMA!"
-     "I LOVE YOU TOO PUMPKIN"
+     "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
