1d7b8ae9015c1fddbe285803724a76f8867b1f8c
Denise Pendleton <dpendl1@hotmail.com>
2018-02-18 22:39:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-    if phrase == phrase.uppercase
+    if phrase == phrase.upcase
       "NO, NOT SINCE 1938!"
 
     elseif phrase == "I LOVE YOU GRANDMA!"
