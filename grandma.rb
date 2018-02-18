31de741beff04b39ce2cdc39c76341e16dc12dc9
Denise Pendleton <dpendl1@hotmail.com>
2018-02-18 22:38:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,17 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+    if phrase == phrase.uppercase
+      "NO, NOT SINCE 1938!"
+    
+    elseif phrase = "I LOVE YOU GRANDMA!"
+     "I LOVE YOU TOO PUMPKIN"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+  end
+    
+  
+
+end
